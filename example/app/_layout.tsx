df8974e500b6ff5fe6e75c57521993d7b30af6fa
Caner Kuru <kurucaner@gmail.com>
2025-01-15 12:35:05 -0500

    smooth toast renamed to smooth push

--- example/app/_layout.tsx
+++ example/app/_layout.tsx
@@ -1,13 +1,13 @@
 import "react-native-reanimated";
 import { Button, StyleSheet, View } from "react-native";
-import { show, SmoothToastProvider } from "@/components";
+import { show, SmoothPushProvider } from "@/components";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={styles.flex}>
       <View style={styles.container}>
-        <SmoothToastProvider />
+        <SmoothPushProvider />
         <Button
           title="Show Success"
           onPress={() => {
